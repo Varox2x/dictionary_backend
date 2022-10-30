e86e7b58b6588fd6c7aa34c3e52bc2e77ba35787
Hubert Rutkowski <hubr2000@gmail.com>
2022-10-30 18:35:32 +0100

    getting words from sets

--- src/routes/sets.js
+++ src/routes/sets.js
@@ -1,7 +1,8 @@
 const {Router} = require('express')
 const router = Router();
-const {sets} = require('../database/models')
+const {sets, words, words_sets} = require('../database/models')
 const setInstance = require('../instances/setInstance')
+const db = require('../database/databse')
 
 router.use((req, res, next) => {
     console.log("Calling sets route middlwear")
@@ -30,5 +31,22 @@ router.get("/", async (req, res) => {
     })
     res.send(setsName)
 })
+
+router.get("/:setName",async (req,res) => {
+    const {setName} = req.params;
+    console.log(setName)
+    if(!setName) return res.sendStatus(422)
+    const userId = req.user.dataValues.id;
+
+    console.log(userId)
+    db.query(`SELECT words.name, words.definition, words.lvl FROM words INNER JOIN words_sets ON words.id = words_sets.word_id  INNER JOIN sets ON words_sets.set_id = sets.id WHERE sets.user_id = ${userId}`,[userId])
+    .then(r => {
+        console.log(r[0])
+        res.send(r[0])
+    })
+    .catch(r => res.sendStatus(422))
+
+})
+
 
 module.exports = router;
